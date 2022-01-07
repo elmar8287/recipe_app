a93fb6b96ab64bfee723f3cb3966a0f9390757db
OLIPLICHE <olipliche@gmail.com>
2022-01-07 15:01:42 +0100

    UPDATE user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+  after_create :set_default_role
+  
+  devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :validatable
+
+  has_many :recipes
+  has_many :foods
+
+  validates :name, presence: true
+
+  private
+
+  def set_default_role
+    update(role: 'user')
+  end
 end
