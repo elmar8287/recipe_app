9779d98d596e9002d7879c0de35aab1bd0a004aa
Elmar <L-mar@inbox.ru>
2022-01-08 14:49:23 +0400

    Generate device use

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   after_create :set_default_role
 
   has_many :recipes, dependent: :destroy
