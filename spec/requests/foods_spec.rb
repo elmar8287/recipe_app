7fe4c0423d6eb0d0f481a8766e7c7fab5aa0a707
Elmar <L-mar@inbox.ru>
2022-01-09 01:28:02 +0400

    Write tests

--- spec/requests/foods_spec.rb
+++ spec/requests/foods_spec.rb
@@ -1,5 +1,3 @@
-
-   
 require 'rails_helper'
 
 RSpec.describe 'Foods', type: :request do
--- spec/requests/recipe_foods_spec.rb
+++ spec/requests/recipe_foods_spec.rb
@@ -1,7 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'RecipeFoods', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  include Devise::Test::IntegrationHelpers
+
+  let(:user) { User.create(name: 'Akasha', email: 'akasha@mail.com', password: 'password') }
+
+  let(:recipe) do
+    Recipe.create(name: 'Chicken filet', description: 'Cook it', preparation_time: '1 hour', cooking_time: '1 hour',
+                  public: true, user: user)
+  end
+
+  let(:recipe_food) { RecipeFood.create(recipe_id: recipe.id) }
+  describe 'GET /new' do
+    before do
+      sign_in user
+      get new_recipe_recipe_food_path(recipe_id: recipe.id)
+    end
+
+    it 'returns http ok' do
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
